d704879a5fc9b00972afdcb3887e07b6ed890262
dixe <niko.fris@gmail.com>
2014-02-06 18:51:43 +0100

    Fixed merged files

--- kernel/hello_syscall.h
+++ kernel/hello_syscall.h
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
+
 #ifndef BUENOS_KERNEL_HELLO__H
 #define BUENOS_KERNEL_HELLO_H
 
 void hello_syscall();
 
 #endif /* BUENOS_KERNEL_HELLO_H */
-=======
-test
->>>>>>> 43f6a2889040aefe65eec8c97dff42b62e0195b9

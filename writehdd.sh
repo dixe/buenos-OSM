191a1cbb300f5a8b901852469724c5bd48b0faa5
dixe <niko.fris@gmail.com>
2014-02-08 15:57:01 +0100

    corrected spelling in writehdd.sh

--- writehdd.sh
+++ writehdd.sh
@@ -1,6 +1,6 @@
 # ~/bin/bash
 # commandline arg should be filename, and the file should
-# be place inside tests/
+# be placed inside tests/
 
 #delete old file from fyams hdd
 util/tfstool delete fyams.harddisk $1

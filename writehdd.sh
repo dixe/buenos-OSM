bc9e162a8d6d88d6ef3f911a881f1cc54a916562
dixe <niko.fris@gmail.com>
2014-02-08 15:55:18 +0100

    added writeshdd scriot

---
+++ writehdd.sh
@@ -0,0 +1,9 @@
+# ~/bin/bash
+# commandline arg should be filename, and the file should
+# be place inside tests/
+
+#delete old file from fyams hdd
+util/tfstool delete fyams.harddisk $1
+
+#write new file
+util/tfstool write fyams.harddisk tests/$1 $1

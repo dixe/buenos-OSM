6dd68e5553fb0e4fcbb2e09a55897892102f8ab7
dixe <niko.fris@gmail.com>
2014-02-17 16:34:03 +0100

    Uploading process.[c|h]

---
+++ proc/process.h
@@ -0,0 +1,108 @@
+/*
+ * Process startup.
+ *
+ * Copyright (C) 2003 Juha Aatrokoski, Timo Lilja,
+ *   Leena Salmela, Teemu Takanen, Aleksi Virtanen.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above
+ *    copyright notice, this list of conditions and the following
+ *    disclaimer in the documentation and/or other materials provided
+ *    with the distribution.
+ * 3. The name of the author may not be used to endorse or promote
+ *    products derived from this software without specific prior
+ *    written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * $Id: process.h,v 1.4 2003/05/16 10:13:55 ttakanen Exp $
+ *
+ */
+
+#ifndef BUENOS_PROC_PROCESS
+#define BUENOS_PROC_PROCESS
+
+#include "lib/types.h"
+
+typedef int process_id_t;
+
+void process_start(const char *executable);
+
+#define USERLAND_STACK_TOP 0x7fffeffc
+
+#define PROCESS_PTABLE_FULL  -1
+#define PROCESS_ILLEGAL_JOIN -2
+
+#define PROCESS_MAX_PROCESSES 32
+
+/* Process state, might need more states*/
+typedef enum {
+  PROCESS_NEW,
+  PROCESS_READY,
+  PROCESS_RUNNING,
+  PROCESS_WAITING,
+  PROCESS_TERMINATED,
+  PROCESS_ZOMBIE
+} process_state_t;
+
+
+/*context for a process*/
+typedef struct {
+    uint32_t cpu_regs[29];   /* The general purpose registers. zero, k0 and 
+                                k1 registers are omitted. */
+    uint32_t hi;             /* The hi register. */
+    uint32_t lo;             /* The lo register. */
+    uint32_t pc;             /* The program counter. Actually loaded from 
+                                EPC register in co-processor 0. */
+    uint32_t status;         /* Status register bits. */
+} proc_context_t;
+
+
+/* Control block structure for processes*/
+typedef struct {
+  /*context hold registers and pc*/
+  proc_context_t *context;
+
+  /*state of the process*/
+  process_state_t state;
+  
+  char *executeable;
+} process_control_block_t;
+
+/* Initialize the process table.  This must be called during kernel
+   startup before any other process-related calls. */
+void process_init();
+
+/* Run process in a new thread. Returns the PID of the new process. */
+process_id_t process_spawn(const char *executable);
+
+/* Stop the process and the thread it runs in. Sets the return value as well */
+void process_finish(int retval);
+
+/* Wait for the given process to terminate, returning its return value. This
+ * will also mark its process table entry as free.
+ * Only works on child processes */
+int process_join(process_id_t pid);
+
+/* Return PID of current process. */
+process_id_t process_get_current_process(void);
+
+/* Return PCB of current process. */
+process_control_block_t *process_get_current_process_entry(void);
+
+#endif
